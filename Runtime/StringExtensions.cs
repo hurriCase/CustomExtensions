90df5793f18952a88d24974d13e61f9d203a7cd8
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-01-20 20:45:03 +0300

    [feat] ConvertToBackingField

---
+++ Runtime/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomExtensions.Runtime
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts a property name to its backing field representation
+        /// </summary>
+        /// <param name="propertyName">The name of the property</param>
+        /// <returns>The backing field name format</returns>
+        public static string ConvertToBackingField(this string propertyName)
+            => $"<{propertyName}>k__BackingField";
+    }
+}
