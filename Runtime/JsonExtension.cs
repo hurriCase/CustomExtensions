e02ba97de5df8ac7d60448b5860dd31d0217b1db
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-01-20 20:45:26 +0300

    [refactor] xml comment for GetJsonPropertyName

--- Runtime/JsonExtension.cs
+++ Runtime/JsonExtension.cs
@@ -6,6 +6,11 @@ namespace CustomExtensions.Runtime
 {
     public static class JsonExtension
     {
+        /// <summary>
+        /// Gets the JSON property name for an enum value, using either the JsonPropertyAttribute
+        /// value or the enum value's string representation.
+        /// </summary>
+        /// <param name="enumValue">The enum value to get the JSON property name for.</param>
         public static string GetJsonPropertyName(this Enum enumValue)
         {
             var attribute = enumValue.GetType()
