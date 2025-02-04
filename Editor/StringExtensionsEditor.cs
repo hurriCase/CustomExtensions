2ecd0d41e108466296a6f9c65c073eb1af97f47a
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-02-04 06:53:14 +0300

    [fix] moved CreateFolder to Editor assembly

---
+++ Editor/StringExtensionsEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+namespace CustomExtensions.Editor
+{
+    public static class StringExtensionsEditor
+    {
+        /// <summary>
+        /// Creates a folder structure in the Unity project based on the provided path.
+        /// </summary>
+        /// <param name="path">The path where the folders should be created, using forward slashes (/) as separators.</param>
+        public static void CreateFolder(this string path)
+        {
+            var folders = path.Split('/');
+            var currentPath = folders[0];
+
+            for (var i = 1; i < folders.Length; i++)
+            {
+                var parentPath = currentPath;
+                currentPath = $"{currentPath}/{folders[i]}";
+
+                if (AssetDatabase.IsValidFolder(currentPath) is false)
+                    AssetDatabase.CreateFolder(parentPath, folders[i]);
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
--- Runtime/StringExtensions.cs
+++ Runtime/StringExtensions.cs
@@ -1,6 +1,4 @@
-﻿using UnityEditor;
-
-namespace CustomExtensions.Runtime
+﻿namespace CustomExtensions.Runtime
 {
     public static class StringExtensions
     {
@@ -11,26 +9,5 @@ namespace CustomExtensions.Runtime
         /// <returns>The backing field name format</returns>
         public static string ConvertToBackingField(this string propertyName)
             => $"<{propertyName}>k__BackingField";
-
-        /// <summary>
-        /// Creates a folder structure in the Unity project based on the provided path.
-        /// </summary>
-        /// <param name="path">The path where the folders should be created, using forward slashes (/) as separators.</param>
-        public static void CreateFolder(this string path)
-        {
-            var folders = path.Split('/');
-            var currentPath = folders[0];
-
-            for (var i = 1; i < folders.Length; i++)
-            {
-                var parentPath = currentPath;
-                currentPath = $"{currentPath}/{folders[i]}";
-
-                if (AssetDatabase.IsValidFolder(currentPath) is false)
-                    AssetDatabase.CreateFolder(parentPath, folders[i]);
-            }
-
-            AssetDatabase.Refresh();
-        }
     }
 }
