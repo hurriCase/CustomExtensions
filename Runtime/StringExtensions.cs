e3e59f11581d83425466a2b47ed58523ea9d8448
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-02-04 06:20:05 +0300

    [feat] CreateFolder extension

--- Runtime/StringExtensions.cs
+++ Runtime/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace CustomExtensions.Runtime
+﻿using UnityEditor;
+
+namespace CustomExtensions.Runtime
 {
     public static class StringExtensions
     {
@@ -9,5 +11,22 @@
         /// <returns>The backing field name format</returns>
         public static string ConvertToBackingField(this string propertyName)
             => $"<{propertyName}>k__BackingField";
+
+        public static void CreateFolder(this string path)
+        {
+            var folders = path.Split('/');
+            var currentPath = folders[0];
+
+            for (var i = 1; i < folders.Length; i++)
+            {
+                var parentPath = currentPath;
+                currentPath = $"{currentPath}/{folders[i]}";
+
+                if (AssetDatabase.IsValidFolder(currentPath) is false)
+                    AssetDatabase.CreateFolder(parentPath, folders[i]);
+            }
+
+            AssetDatabase.Refresh();
+        }
     }
 }
