bb406f5b41d729a55d23daca538d96d9dce3bd47
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-02-08 06:31:55 +0300

    [refactor] moved ConvertToBackingField to Editor extension

--- Editor/StringExtensionsEditor.cs
+++ Editor/StringExtensionsEditor.cs
@@ -24,5 +24,13 @@ namespace CustomExtensions.Editor
 
             AssetDatabase.Refresh();
         }
+
+        /// <summary>
+        /// Converts a property name to its backing field representation
+        /// </summary>
+        /// <param name="propertyName">The name of the property</param>
+        /// <returns>The backing field name format</returns>
+        public static string ConvertToBackingField(this string propertyName)
+            => $"<{propertyName}>k__BackingField";
     }
 }
