de66df883157c91467633af46c076dade3fdff75
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-02-08 06:32:06 +0300

    [feat] ReflectionExtensionsEditor

---
+++ Editor/ReflectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CustomExtensions.Editor
+{
+    internal static class ReflectionExtensionsEditor
+    {
+        public static string GetFieldName<TClass, TField>(this TClass _,
+            BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance)
+            where TClass : class =>
+            typeof(TClass)
+                .GetFields(bindingFlags)
+                .First(fieldInfo => fieldInfo.FieldType == typeof(TField))
+                .Name;
+
+        public static string GetListFieldName<TClass, TElement>(this TClass _,
+            BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance)
+            where TClass : class =>
+            typeof(TClass)
+                .GetFields(bindingFlags)
+                .First(fieldInfo => fieldInfo.FieldType == typeof(List<TElement>))
+                .Name;
+    }
+}
